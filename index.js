3758f925b45d35f4ca2d50ba3a9fc7496997d01e
Nikolaus Piccolotto <npiccolotto@Nikolauss-MacBook-Pro.local>
2017-12-30 15:02:06 +0100

    Make it work, add perf measure

--- index.js
+++ index.js
@@ -2,21 +2,36 @@ import wasmBinary from './zxing/build/zxing.wasm';
 let MODULE = null;
 
 function loadModule() {
-  return import('./zxing/build/zxing.js').then(Module => (MODULE = Module));
+  return new Promise(resolve =>
+    import('./zxing/build/zxing.js').then(
+      Module => (Module.postRun = [() => resolve(Module)])
+    )
+  ).then(m => (MODULE = m));
 }
+const perf = window.performance;
 
 export function read(eightBitGrayPixelArray, imageWidth, imageHeight) {
-  return new Promise(resolve => {
-    const preparation = Promise.resolve();
-    if (MODULE === null) {
-      preparation = loadModule();
-    }
-    return preparation.then(() => {
-      var ptr = MODULE._malloc(eightBitGrayPixelArray.length);
-      MODULE.HEAPU8.set(eightBitGrayPixelArray, ptr);
-      const result = MODULE.read_code(ptr, imageWidth, imageHeight);
-      MODULE._free(ptr);
-      return result;
-    });
+  let preparation = Promise.resolve();
+  if (MODULE === null) {
+    preparation = loadModule();
+  }
+  return preparation.then(() => {
+    var ptr = MODULE._malloc(eightBitGrayPixelArray.length);
+    MODULE.HEAPU8.set(eightBitGrayPixelArray, ptr);
+    const t1 = perf.mark('t1');
+    const result = MODULE.read_code(ptr, imageWidth, imageHeight);
+    const t2 = perf.mark('t2');
+    MODULE._free(ptr);
+    perf.measure('read', 't1', 't2')
+    // Get all of the measures out.
+    // In this case there is only one.
+    var measures = perf.getEntriesByName('read');
+    var measure = measures[0];
+    console.log('perf milliseconds:', measure.duration);
+
+    // Clean up the stored markers.
+    perf.clearMarks();
+    perf.clearMeasures();
+    return result;
   });
 }
